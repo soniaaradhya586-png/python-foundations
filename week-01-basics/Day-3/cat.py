ecf66604e5c6cf88b607730ed998e91683148ca3
Aaradhya soni <soniaaradhya586@gmail.com>
2026-04-10 20:54:33 +0530

    Week-1; Day-3: Learned about Loops & Lists

---
+++ week-01-basics/Day-3/cat.py
@@ -0,0 +1,50 @@
+#i = 0
+#while i < 4
+ #   print("Meow")
+#    i += 1
+
+
+#for _ in range(3):
+#    print("Meow")
+
+#print("Meow\n" * 3, end="")
+
+#while True:
+#   n = int(input("What's n? "))
+#    if 0 < n:
+#        break
+#
+#for _ in range(n):
+#   print("Meow")
+
+def main():
+    number = get_number()
+    meow(number)
+
+def get_number():
+    while True:
+        n = int(input("Waht's n?"))
+        if n> 0:
+            break
+    return n
+
+def meow(number):
+    for _ in range(number):
+        print("Meow")
+
+
+meow()
+
+                
+                
+
+
+
+
+
+
+
+
+
+
+                
