0b411ad071e3e725f4a10d04cfe1b4c86a8700a2
Aaradhya soni <soniaaradhya586@gmail.com>
2026-04-11 19:37:36 +0530

    learned len, dict, and None

--- week-01-basics/Day-4/hogwarts.py
+++ week-01-basics/Day-4/hogwarts.py
@@ -1,4 +1,22 @@
-students = ["Harry", "Ron", "Hermione", "Draco", "Neville"]
+#students = ["Harry", "Ron", "Hermione", "Draco", "Neville"]
 
-for students in students:
-    print(students, end="\n                             ")
+#for students in students:
+#    print(students)
+
+#for i in range(len(students)): 
+#    print(i + 1, students[i])
+
+#houses = ["Gryffindor", "Gryffindor", "Griffindor", "Slytherin"]
+
+students = {
+    "Harry": "Gryffindor",
+    "Ron": "Gryffindor",
+    "Hermione": "Gryffindor",
+    "Draco": "Slytherin",
+    "Neville": "Gryffindor"
+}
+
+#print(students["Harry"]) 
+
+for student in students:
+    print(student, students[student])
