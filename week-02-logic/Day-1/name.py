0e12c7ac939896cc6d0675481a8a2af1715f89a1
Aaradhya soni <soniaaradhya586@gmail.com>
2026-04-15 20:14:28 +0530

    used sys.argv with if, else

--- week-02-logic/Day-1/name.py
+++ week-02-logic/Day-1/name.py
@@ -1,3 +1,14 @@
 import sys
 
-print("hello, my name is", sys.argv[1])
+#try:
+#    print("hello, my name is", sys.argv[1])
+#xcept IndexError:
+#    print("Too few arguments")
+
+
+if len(sys.argv) < 2:
+    print("Too few arguments")
+elif len(sys.argv) > 2:
+    print("Too many arguments")
+else:
+    print("Hello, my name is", sys.argv[1])
