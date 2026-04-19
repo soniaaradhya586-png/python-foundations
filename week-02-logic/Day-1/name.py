ff251b6050f30496f14ba1380f6eff78d9b15efa
Aaradhya soni <soniaaradhya586@gmail.com>
2026-04-19 19:35:13 +0530

    learned about packages, pip, and slices

--- week-02-logic/Day-1/name.py
+++ week-02-logic/Day-1/name.py
@@ -7,8 +7,9 @@ import sys
 
 
 if len(sys.argv) < 2:
-    print("Too few arguments")
-elif len(sys.argv) > 2:
-    print("Too many arguments")
-else:
-    print("Hello, my name is", sys.argv[1])
+   sys.exitprint("Too few arguments")
+#elif len(sys.argv) > 2:
+#    sys.exitprint("Too many arguments")
+#   print("Hello, my name is", sys.argv[1])
+for arg in sys.argv[1:]:
+   print("hello, my name is", arg)
