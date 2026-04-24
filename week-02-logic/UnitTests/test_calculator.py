38b7f150af674c761b1f31e8bef4bd227507ddcf
Aaradhya soni <soniaaradhya586@gmail.com>
2026-04-24 10:25:41 +0530

    changed file test_calculator.py to show more details in showing failures

--- week-02-logic/UnitTests/test_calculator.py
+++ week-02-logic/UnitTests/test_calculator.py
@@ -1,34 +1,42 @@
-from calculator import square
+#    test_square()
 
-def main():
-    test_square()
-
-def test_square():
+#def test_square():
      # if square(2) != 4:
      #    print("2 squared was not 4")
      # if square(3) != 9:
      #    print("3 squired waas not 9")
-    try:
-         assert square(2) == 4
-    except AssertionError:
-         print("2 squared was not 4")
-    try:
-        assert square(3) == 9
-    except AssertionError:
-        print("3 squared was not 9")
-    try:
-         assert square(-2) == 4
-    except AssertionError:
-         print("-2 squared was not 4")
-    try:
-        assert square(-3) == 9
-    except AssertionError:
-        print("-3 squared was not 9")
-    try:
-        assert square(0) == 0
-    except AssertionError:
-        print("0 squared was not 0") 
+ #        assert square(2) == 4
+  #  except AssertionError:
+ #        print("2 squared was not 4")
+ #   try:
+ #       assert square(3) == 9
+ #   except AssertionError:
+ #       print("3 squared was not 9")
+ #   try:
+ #        assert square(-2) == 4
+ #   except AssertionError:
+ #        print("-2 squared was not 4")
+ #   try:
+ #       assert square(-3) == 9
+ #       print("-3 squared was not 9")
+ #    try:
+ #       assert square(0) == 0
+ #   except AssertionError:
+ #       print("0 squared was not 0") 
 
 
-if __name__ == "__main__":
-    main()      
+#if __name__ == "__main__":
+#   main()      
+from calculator import square
+
+def test_square():
+    assert square(2) == 4
+    assert square(3) == 9
+
+
+def test_negatuve():
+    assert square(-2) == 4
+    assert square(-3) == 9
+
+def test_zero():
+    assert square(0) == 0
