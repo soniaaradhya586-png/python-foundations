df2ea1e464806e2dac8e9ae68fdbf7614c716f04
Aaradhya soni <soniaaradhya586@gmail.com>
2026-04-26 15:18:33 +0530

    learned how to use dictwriter & dictreader

--- week-02-logic/FilesInputOutput/students.py
+++ week-02-logic/FilesInputOutput/students.py
@@ -1,20 +1,23 @@
+import csv
+
+
 #with open("students.csv") as file:
 #    for line in file:
 #        row = line.rstrip().split(",")
 #        print(f"Name: {row[0]}, House: {row[1]}")
 
-students = []
+####students = []
 
-with open("students.csv") as file:
-    for line in file:
-        name, house = line.rstrip().split(",")
+####with open("students.csv") as file:
+####    for line in file:
+####        name, house = line.rstrip().split(",")
        # students.append(f"{name} is in {house}")
         ##student = {}
         ##student["name"] = name
         ##student["house"] = house
         ### same thing as above can be achived just by doing this 
-        student = {"name": name, "house": house}
-        students.append(student)
+####        student = {"name": name, "house": house}
+####        students.append(student)
 
 #for students in sorted(students):
 #    print(students)
@@ -24,5 +27,23 @@ with open("students.csv") as file:
 #    return student["name"]
 
 
-for student in sorted(students, key= lambda student: student["name"]):
-    print(f"{student['name']} is in {student['house']}")
+#####students = []
+
+#####with open("students.csv") as file:
+#####    reader = csv.reader(file)
+#####    for name, home in reader:
+#####        students.append({"name": name, "home": home})
+
+
+#####for student in sorted(students, key= lambda student: student["name"]):
+#####    print(f"{student['name']} is from {student['home']}")
+
+name = input("What's your name? ")
+home = input("Where's your house? ")
+
+with open("students.csv", "a", newline="") as file:
+    #writer = csv.writer(file)
+    #writer.writerow([name, home])
+    writer = csv.DictWriter(file, fieldnames=["name", "home"])
+    writer.writerow({"name": name, "home": home})
+    
