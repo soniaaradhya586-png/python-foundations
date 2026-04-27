e60550b6be592a7e91aa8e113abbe89d80f67695
Aaradhya soni <soniaaradhya586@gmail.com>
2026-04-27 11:20:58 +0530

    learned how to use simple Regular Expressions and 're' library

---
+++ week-03/RegularExpressions/validate.py
@@ -0,0 +1,25 @@
+import re
+
+
+email = input("What's your email? ").strip()
+
+# using "@" and "." to determine that the input is an email or not
+##if "@" in email and "." in email:
+##    print("valid")
+##else:
+##    print("Invalid")
+
+
+###username, domain = email.split("@")
+
+#if (username) and ("." in domain):
+###if (username) and (domain.endswith(".edu")):
+###    print("Valid")
+###else:
+###    print("Invalid")
+
+
+if re.search(r"^.*@.*\.com$", email):
+    print("Valid")
+else:
+    print("Invalid")
