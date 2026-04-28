0b7a873dcf952a2a057b2c42609378902cd9ac8e
Aaradhya soni <soniaaradhya586@gmail.com>
2026-04-28 17:17:23 +0530

    learned some more functions of re library

--- week-03/RegularExpressions/format.py
+++ week-03/RegularExpressions/format.py
@@ -7,11 +7,14 @@ name = input("What's your name? ").strip()
 #    name = f"{first} {last}"
 #print(f"hello, {name}")
 
-matches = re.search(r"^(.+), *(.+)$", name)
-if matches:
+##matches = re.search(r"^(.+), *(.+)$", name)
+##if matches:
     ##last, first = matches.groups()
     #last = matches.group(1)
     #first = matches.group(2)
-    name = matches.group(2) + " " + matches.group(1)
-print(f"hello, {name}")
+##    name = matches.group(2) + " " + matches.group(1)
+##print(f"hello, {name}")
 
+if matches := re.search(r"^(.+), *(.+)$", name):
+    name = matches.group(2) + "" + matches.group(1)
+print(f"hello, {name}")
---
+++ week-03/RegularExpressions/twitter.py
@@ -0,0 +1,12 @@
+import re
+
+url = input("URL: ").strip()
+
+#username = url.removeprefix("https://twitter.com/", "")
+#print(f"username: {username}")
+
+##username = re.sub(r"^(https://)?(www\.)?twitter\.com/", "",url)
+##print(f"Username: {username}")
+
+if matches := re.search(r"^https?://(?:www.\.)?twitter\.com/([a-z0-9_]+)$", url, re.IGNORECASE):
+    print(f"Username:", matches.group(1))
