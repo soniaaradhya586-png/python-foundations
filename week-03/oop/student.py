e067a5978a69bd961b884a35dea9c19ea0f9a473
Aaradhya soni <soniaaradhya586@gmail.com>
2026-05-02 17:42:28 +0530

    learnd how to use classes in python

--- week-03/oop/student.py
+++ week-03/oop/student.py
@@ -1,3 +1,27 @@
+class Student:
+    def __init__(self, name, house):
+          if not name:
+                raise ValueError("Missing name")
+          if house not in ["Griffindor", "Huffelpuff", "Ravenclaw", "Slytherin"]:
+                raise ValueError("Invalid house")
+          self.name = name
+          self.house = house
+          #self.patronus = patronus
+
+    def __str__(self):
+          return f"{self.name} from {self.house}"
+
+    #def charm(self):
+    #      match self.patronus:
+    #           case "Stag":
+    #                  return "horse"
+    #            case "Otter":
+    #                  return "shoe"
+    #            case "Jack Russell terrier":
+    #                  return "cute dog"
+    #                 return "wand"
+
+
 ##def main():
     #name = get_name()
     #house = get_house()
@@ -11,15 +35,15 @@
 #def get_house():
  #       return input("House: ")
 
-def main():
-        student =get_student()
+###def main():
+###        student =get_student()
         ##if student[0] == "Padma":
         ##     student[1] = "Ravenclaw"
         #print(f"{student[0]} from {student[1]}")
                   #usind dictionarines to change the string
-        if student["name"] == "padma":
-                student["house"] = "Ravenclaw"
-        print(f"{student['name']} from {student['house']}")
+###        if student["name"] == "Padma":
+###                student["house"] = "Ravenclaw"
+###        print(f"{student['name']} from {student['house']}")
 
 
 #def get_student():
@@ -33,11 +57,27 @@ def main():
 ##       student["house"] = input("House: ")
 ##       return student
 
-def get_student():
-       name = input("Name: ")
-       house = input("House: ")
-       return{"name": name, "house": house}
+###def get_student():
+###       name = input("Name: ")
+###       house = input("House: ")
+###       return{"name": name, "house": house}
 
+def main():
+      student = get_student()
+      print(f"{student.name} from {student.house}")
+
+
+def get_student():
+     #student = Student
+     #student.name = input("Name: ")
+     #student.house = input("House: ")
+     name = input("Name: ")
+     house = input("House: ")
+     #patronus = input("Patronus: ") 
+     return Student(name, house)
 
 if __name__ == "__main__":
-        main()
+      Student = get_student()
+      #print("Expecto Partonum!")
+      #print(student.charm())
+      print(Student)
