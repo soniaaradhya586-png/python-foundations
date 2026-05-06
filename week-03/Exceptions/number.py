57b4d7637bfe1306b70e08e117429d6cfc759643
Aaradhya soni <soniaaradhya586@gmail.com>
2026-05-06 20:09:22 +0530

    learned how to catch errors and tell users to not do that

---
+++ week-03/Exceptions/number.py
@@ -0,0 +1,20 @@
+def main():
+    x = get_int("What's x?")
+    print(f"x is {x}")
+     
+
+def get_int(prompt):
+    while True:
+        try:
+            return int(input(prompt))
+        except ValueError:
+            #print("x is not an integer")
+            pass
+         #else:
+         #    break
+         ##else:
+         ##  return x
+
+
+
+main()
