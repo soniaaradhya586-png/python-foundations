da2e92cd7be258c90f0c7580e3a97d8e007556a9
Aaradhya soni <soniaaradhya586@gmail.com>
2026-05-08 16:11:42 +0530

    learned how to use map in python

---
+++ week-03/EtCetera/yell.py
@@ -0,0 +1,16 @@
+def main():
+    yell("This", "is", "CS50")
+
+
+#def yell(*words):
+#    uppercased = []
+#    for word in words:
+#        uppercased.append(word.upper())
+#    print(*uppercased)
+
+def yell(*words):
+    uppercased = map(str.upper, words)
+    print(*uppercased)
+
+if __name__ == "__main__":
+    main()
